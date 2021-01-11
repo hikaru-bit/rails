1ad2972a3859be68f6477fa665ad3eebc0877916
EC2 Default User <ec2-user@ip-172-31-6-145.ec2.internal>
2021-01-11 04:59:14 +0000

    [finish] 4-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,3 +1,29 @@
 class TodolistsController < ApplicationController
-  @list = List.new
+  def new
+    @list = List.new
+  end
+
+  def create
+      list = List.new(list_params)
+      # １. データを新規登録するためのインスタンス作成
+      list.save
+      # ２. データをデータベースに保存するためのsaveメソッド実行
+      redirect_to '/top'
+      # ３. トップ画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  # フォームからデータを送信するときは、「マスアサインメント脆弱性」というセキュリティ上の問題があります。
+  # （簡単に伝えると、データ送信時に不正なリクエストによって、予期しない値を変更されてしまう脆弱性です）
+  # Railsでは、この脆弱性を防ぐ「ストロングパラメータ」の仕組みが用意されています。 
+   # この仕組みがprivateの行から下のコードになります。
+  #  privateは一種の境界線で、「ここから下はcontrollerの中でしか呼び出せません」という意味があります。
+  # そのため、他のアクション（index,show,createなど）を巻き込まないようにprivateはControllerファイルの一番下のendのすぐ上に書いて下さい。
+
+
+  def list_params
+      params.require(:list).permit(:title, :body)
+    #ストロングパラメータの構文はこのようにおおよそ決まっている   
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
+  # todolistsページにアクセスした際にnewアクション(空のクラスがモデルに設定)が実行
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  # getメソッドでtopをクリックしたらhomesコントローラーのtopアクションが呼び出される
+  post 'todolists' => 'todolists#create'
+  # todolistsのURLにPOSTメソッドでアクセスしたときにtodolistsコントローラーのcreateアクションが呼び出される 
 end
